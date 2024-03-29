489b4031fda4031d01c7fa65803cdd07a03428c9
unknown <souviktechno98@gmail.com>
2024-03-29 20:08:21 +0530

    CORS added

--- TeleRadiology/src/main/java/com/example/TeleRadiology/controller/ReportController.java
+++ TeleRadiology/src/main/java/com/example/TeleRadiology/controller/ReportController.java
@@ -2,12 +2,7 @@ package com.example.TeleRadiology.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.TeleRadiology.domain.services.EmailOtpService;
 import com.example.TeleRadiology.domain.services.ReportService;
@@ -25,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/teleRadiology")
+@CrossOrigin(originPatterns = "*localhost*")
 @RequiredArgsConstructor
 public class ReportController {
     private final ReportService repService;
