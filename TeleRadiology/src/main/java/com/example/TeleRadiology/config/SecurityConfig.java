4a8b85c35b4eb5ea277f1ad31ab6e8936ac80b68
Sumith1908 <sumith.hegde@iiitb.ac.in>
2024-04-04 23:26:36 +0530

    permit all for jwt for simplicity

--- TeleRadiology/src/main/java/com/example/TeleRadiology/config/SecurityConfig.java
+++ TeleRadiology/src/main/java/com/example/TeleRadiology/config/SecurityConfig.java
@@ -24,12 +24,12 @@ public class SecurityConfig {
         http.csrf(config -> config.disable());
         http.authorizeHttpRequests(
                 configure -> configure
-                        // .requestMatchers("/teleRadiology/loginCredentials").permitAll()
+                        .requestMatchers("/teleRadiology/loginCredentials").permitAll()
                         // .requestMatchers("/teleRadiology/createPatientCred").permitAll()
                         // .requestMatchers("/teleRadiology/otpVerification/*").permitAll()
                         // .requestMatchers("/test").authenticated()
-                        // .anyRequest().authenticated());
-                        .anyRequest().permitAll());
+                        .anyRequest().authenticated());
+        // .anyRequest().permitAll());
         http.httpBasic(Customizer.withDefaults());
 
         http.addFilterBefore(jwtFilter, BasicAuthenticationFilter.class);
