6728a1de6ab17f4708432c452d7bd5ea8728e50d
Harsh Kela <harsheeed@gmail.com>
2024-04-11 18:13:24 +0530

    Notification api

--- TeleRadiology/src/main/java/com/example/TeleRadiology/data/dao/AnnotationNotificationDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.TeleRadiology.data.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.TeleRadiology.data.entities.AnnotationNotificationEntity;
-
-public interface AnnotationNotificationDao extends JpaRepository<AnnotationNotificationEntity, Integer> {
-
-}
---
+++ TeleRadiology/src/main/java/com/example/TeleRadiology/domain/model/Notification.java
@@ -0,0 +1,24 @@
+package com.example.TeleRadiology.domain.model;
+
+import com.example.TeleRadiology.data.entities.CredentialsEntity;
+import com.example.TeleRadiology.data.entities.DoctorEntity;
+import com.example.TeleRadiology.data.entities.RadiologistEntity;
+import com.example.TeleRadiology.data.entities.ReportEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+
+public class Notification {
+     private int doctorId;
+     private int radiologistId;
+     private int reportId;
+
+    
+}
